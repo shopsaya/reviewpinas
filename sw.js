d6e520365ed1150e43000fe134abd3d07b732125
shopsaya <shopsayaph1080@gmail.com>
2026-07-08 13:23:29 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Simple offline-first shell cache. Bump VERSION on each deploy to refresh.
-const VERSION = "reviewpinas-v10";
+const VERSION = "reviewpinas-v11";
 const SHELL = ["/", "/index.html", "/manifest.webmanifest"];
 
 self.addEventListener("install", (e) => {
